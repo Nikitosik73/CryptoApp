cbe28f9b3d6dff6cfbf4d6e153970cf819bb6d9f
nikita <paramonov20@bk.ru>
2023-05-04 17:14:05 +0300

    Установил слушатель клика для адаптера с помощью лямбда выражения

--- app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
@@ -1,20 +1,19 @@
 package com.example.cryptoapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.cryptoapp.adapter.CoinInfoAdapter
 import com.example.cryptoapp.databinding.ActivityCoinPriceListBinding
-import com.example.cryptoapp.pojo.priceinfo.CoinPriceInfo
 import com.example.cryptoapp.viewmodel.CoinViewModel
 
 class CoinPriceListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCoinPriceListBinding
     private lateinit var viewModel: CoinViewModel
+    private lateinit var coinAdapter: CoinInfoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,23 +21,21 @@ class CoinPriceListActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val adapter = CoinInfoAdapter(this)
-        adapter.onClickCoinListener = object : CoinInfoAdapter.OnClickCoinListener {
-            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
-                val intent = CoinDetailActivity.newIntent(
-                    this@CoinPriceListActivity,
-                    coinPriceInfo.fromSymbol
-                )
-                startActivity(intent)
-            }
+        coinAdapter = CoinInfoAdapter(this)
+        coinAdapter.onCoinCLickListener = {
+            val intent = CoinDetailActivity.newIntent(
+                this@CoinPriceListActivity,
+                it.fromSymbol
+            )
+            startActivity(intent)
         }
-        binding.recyclerViewCoinInfo.adapter = adapter
+        binding.recyclerViewCoinInfo.adapter = coinAdapter
 
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
 
         viewModel.priceList.observe(this, Observer {
             Log.d("Test_of_load_data", it.toString())
-            adapter.coinPriceList = it
+            coinAdapter.coinPriceList = it
         })
     }
 }
