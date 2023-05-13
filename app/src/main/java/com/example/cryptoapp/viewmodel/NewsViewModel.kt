678d3b468edd56af400ede538f86baa308eaedaa
nikita <paramonov20@bk.ru>
2023-05-13 14:59:48 +0300

    Создал viewModel для NewsCoinActivity.kt

---
+++ app/src/main/java/com/example/cryptoapp/viewmodel/NewsViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.cryptoapp.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.cryptoapp.api.ApiFactory
+import com.example.cryptoapp.pojo.news.Data
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class NewsViewModel : ViewModel() {
+
+    private val compositeDisposable = CompositeDisposable()
+
+    private var _newsList = MutableLiveData<List<Data>>()
+    val newsList: LiveData<List<Data>>
+        get() = _newsList
+
+    init {
+        loadNews()
+    }
+
+    private fun loadNews() {
+        val disposable = ApiFactory.apiService.getNewsCoin()
+            .map { it.data?: throw RuntimeException("Empty List") }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("NewsViewModel", "Success: $it")
+                _newsList.value = it
+            }, {
+                Log.d("NewsViewModel", "Failure: ${it.message}")
+            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
