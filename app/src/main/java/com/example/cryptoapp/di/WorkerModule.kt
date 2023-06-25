c932b2d741209d1da1089379aad05c4a74eb0273
nikita <paramonov20@bk.ru>
2023-06-25 04:03:02 +0300

    Научился инжектить зависимости в multiple worker

---
+++ app/src/main/java/com/example/cryptoapp/di/WorkerModule.kt
@@ -0,0 +1,19 @@
+package com.example.cryptoapp.di
+
+import com.example.cryptoapp.data.workers.ChildWorkerFactory
+import com.example.cryptoapp.data.workers.RefreshDataWorker
+import com.example.cryptoapp.di.annotation.WorkerKey
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+interface WorkerModule {
+
+    @Binds
+    @IntoMap
+    @WorkerKey(RefreshDataWorker::class)
+    fun bindRefreshWorkerFactory(
+        worker: RefreshDataWorker.Factory
+    ): ChildWorkerFactory
+}
